371052963b9e35f47bcbf8f18421e937b9281983
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-05 03:56:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,11 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.any? {|name| name.length>=4}
+  if array.any? {|name| name.length>=4}
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
