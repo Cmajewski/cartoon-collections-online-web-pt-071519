407592bace2c2d1dbf89770a0573d5232f994225
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-05 03:27:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_name)# code an argument here
+dwarf_name.each_with_index do |dwarf,index|
+  puts "#{index+1}. #{dwarf}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.each.to_a.collect do |name|
+    "#{name.upcase}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
